745cfae85a7f6e1ce731914d3429e42879f72b2d
Weslley Caetano <weslleycaetanosoares@gmail.com>
2026-01-19 14:35:20 -0300

    feat: add route to send past month pdf reports automatically

--- src/MoneyScope.Api/Controllers/ReportPdfController.cs
+++ src/MoneyScope.Api/Controllers/ReportPdfController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MoneyScope.Application.Interfaces;
+using MoneyScope.Application.Models.User;
+using MoneyScope.Application.Services;
+using MoneyScope.Core.Models;
 namespace MoneyScope.Api.Controllers
 {
     /// <summary>
@@ -18,14 +21,63 @@ namespace MoneyScope.Api.Controllers
         {
             _reportPdfService = reportPdfService;
         }
+       
+        [HttpGet("relatorio-mensal")]
+        [ProducesResponseType(typeof(FileContentResult), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GenerateMonthlyReport(
+            [FromQuery] long userId,
+            [FromQuery] int month,
+            [FromQuery] int year
+        )
+        {
+            try
+            {
+                var pdf = await _reportPdfService.GenerateMonthlyReportAsync(userId, month, year);
+
+                return File(
+                    pdf,
+                    "application/pdf",
+                    $"relatorio-{month}-{year}.pdf"
+                );
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("enviar-relatorio-mensal")]
+        [ProducesResponseType(typeof(ResponseModel<dynamic>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GenerateAndSendMonthlyReport(
+            [FromQuery] long userId,
+            [FromQuery] int month,
+            [FromQuery] int year
+        )
+        {
+            try
+            {
+                var pdf = await _reportPdfService.GenerateAndSendMonthlyReportAsync(userId, month, year);
+
+                // Retorna sucesso + opção de download
+                return File(
+                    pdf,
+                    "application/pdf",
+                    $"relatorio-{month}-{year}.pdf"
+                );
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
         /// <summary>
-        /// Gera e envia por email o relatorio financeiro mensal em PDF.
+        /// Adiciona um usuario.
         /// </summary>
-        /// <param name="month">Mês do relatório.</param>
-        /// <param name="year">Ano do relatório.</param>
         /// <returns></returns>
-        [HttpPost("GenerateAndSendMonthlyReport")]
-        public async Task<IActionResult> GenerateAndSendMonthlyReport([FromQuery] long userId, int month, int year) =>
-             Result(await _reportPdfService.GenerateMonthlyReportAsync(userId, month, year));
+        //[APIAuthorization(new string[] { "Users-C" })]
+        [HttpPost("BackgroundTest")]
+        public async Task<IActionResult> Add() =>
+             Result(await _reportPdfService.SendMonthlyReportsToAllUsersAsync());
     }
 }
