9f18a47cd8f3c1f06c34a4b292cf1535cb8ae41a
Weslley Caetano <weslleycaetanosoares@gmail.com>
2026-01-19 12:01:54 -0300

    feat: add hangfire module

---
+++ src/MoneyScope.Api/Extensions/HangFireDashboard.cs
@@ -0,0 +1,25 @@
+﻿using Hangfire.Dashboard.BasicAuthorization;
+
+namespace MoneyScope.Api.Extensions
+{
+    public class HangFireDashboard
+    {
+        public static BasicAuthAuthorizationFilter[] AuthAuthorizationFilters()
+        {
+            return [new BasicAuthAuthorizationFilter(new BasicAuthAuthorizationFilterOptions{
+        SslRedirect = false,
+
+        RequireSsl = false,
+        LoginCaseSensitive = true,
+
+        Users = new []{
+            new BasicAuthAuthorizationUser{
+                Login = "admin",
+                PasswordClear = "admin"
+            }
+        }
+    }){
+    }];
+        }
+    }
+}
---
+++ src/MoneyScope.Application/Services/ServiceBackGround.cs
@@ -0,0 +1,56 @@
+﻿using Hangfire;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyScope.Application.Services
+{
+    public class ServiceBackGround : BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ServiceBackGround(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("Executou o agendamento dos bgServices");
+            var tz = TimeZoneInfo.FindSystemTimeZoneById(
+             OperatingSystem.IsWindows()
+                 ? "E. South America Standard Time"
+                 : "America/Sao_Paulo"
+             );
+
+            var options = new RecurringJobOptions
+            {
+                TimeZone = tz
+            };
+
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                //var _recurringJobManager = scope.ServiceProvider.GetRequiredService<IRecurringJobManager>();
+                //_recurringJobManager.AddOrUpdate<IKanbanService>(
+                //    "busca-kanbans-protheus",
+                //    service => service.BuscaKanbansProtheus(),
+                //    Cron.Daily(3),
+                //    options
+                //);
+
+                //_recurringJobManager.AddOrUpdate<IUsuarioService>(
+                //    "busca-usuarios-protheus",
+                //    service => service.BuscaUsuariosProtheus(),
+                //    Cron.Daily(3, 20),
+                //    options
+                //);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
